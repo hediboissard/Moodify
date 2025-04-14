ce43b15d13467359be0905232448d517a1da5cbc
Jules Dupont <jules.dupont0709@gmail.com>
2025-04-14 11:06:57 +0200

    initialize Vue 3 project with Vite and set up backend with Express

---
+++ BackMoodify/src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur le backend Node.js avec .env !');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
