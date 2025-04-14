89d5c82528dc430b10c2e3aa2e83f913d4e476d7
Ace <ace92dev@gmail.com>
2025-04-14 14:30:20 +0200

    Ajout de la LoginPage

--- FrontMoodify/src/router/index.js
+++ FrontMoodify/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import LoginPage from "../views/LoginPage.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
+  { path: "/", name: "Login", component: LoginPage },
   { path: "/about", name: "About", component: About },
+  { path: "/home", name: "Home", component: Home }
 ];
 
 const router = createRouter({
---
+++ FrontMoodify/src/views/LoginPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-black">
+    <div class="w-full max-w-sm text-center text-green-500 space-y-5">
+      <h1 class="text-4xl font-bold">Moodify</h1>
+
+      <button class="w-full border border-green-500 rounded-lg py-2 hover:bg-green-600 hover:text-black transition">
+        Create account
+      </button>
+
+      <div class="flex items-center justify-center space-x-4">
+        <hr class="border-green-500 flex-1" />
+        <span class="text-green-500">or</span>
+        <hr class="border-green-500 flex-1" />
+      </div>
+
+      <button class="w-full border border-green-500 rounded-lg py-2 flex items-center justify-center space-x-2 hover:bg-green-600 hover:text-black transition">
+        <svg class="w-5 h-5" viewBox="0 0 168 168" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="..."/>
+        </svg>
+        <span>Continue with Spotify</span>
+      </button>
+
+      <input
+        type="text"
+        placeholder="Email or username"
+        class="w-full border border-green-500 rounded-lg py-2 px-4 bg-black text-green-500 placeholder-green-500 focus:outline-none focus:ring-2 focus:ring-green-600"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="w-full border border-green-500 rounded-lg py-2 px-4 bg-black text-green-500 placeholder-green-500 focus:outline-none focus:ring-2 focus:ring-green-600"
+      />
+
+      <button
+        @click="goToHome"
+        class="w-full bg-green-500 text-black font-semibold py-2 rounded-lg hover:bg-green-600 transition"
+      >
+        Log in
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goToHome() {
+  router.push('/home')
+}
+</script>
