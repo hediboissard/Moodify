278209ed1c0b7f7d1fc47ba9bd083c9431c5ef9a
Matéo Journiac <journiacmateo@gmail.com>
2025-04-14 11:46:04 +0200

    Init Home + Navbar

---
+++ FrontMoodify/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <Navbar />
+      <main style="padding: 2rem;">
+        <h1>Bienvenue sur la page d'accueil</h1>
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+  import Navbar from '@/components/Navbar.vue';
+  </script>
+  
