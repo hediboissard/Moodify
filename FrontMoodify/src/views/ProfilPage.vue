8cd51ae907469a10ce045a846f4eb18f15354a82
Ace <ace92dev@gmail.com>
2025-04-15 11:08:23 +0200

    Ajout de la page friends

--- FrontMoodify/src/views/ProfilPage.vue
+++ FrontMoodify/src/views/ProfilPage.vue
@@ -1,37 +1,37 @@
 <template>
-    <Navbar />
-    <div class=" bg-neutral-900 text-white flex flex-col items-center">
-  
-      <div class="mt-12">
-        <img src="https://www.svgrepo.com/show/382106/profile-avatar.svg"
-             alt="Profile"
-             class="w-32 h-32 rounded-full border-2 border-gray-600" />
-      </div>
-  
-      <div class="mt-10 space-y-6 text-lg">
-        <p><span class="font-bold">Pseudo:</span> {{ user.pseudo }}</p>
-        <p><span class="font-bold">Name:</span> {{ user.name }}</p>
-        <p><span class="font-bold">Surname:</span> {{ user.surname }}</p>
-      </div>
-  
-      <button class="mt-12 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-6 rounded-lg">
-        Account Delete
-      </button>
-    </div>
-  </template>
-  
-  <script setup>
-  import { useRouter } from 'vue-router'
-  import Navbar from '@/components/Navbar.vue';
+  <Navbar />
+  <div class=" bg-neutral-900 text-white flex flex-col items-center">
 
-  const user = {
-    pseudo: 'vincentleb',
-    name: 'Vincent',
-    surname: 'Lebel'
-  }
-  const router = useRouter()
-  
-  function goToHome() {
-    router.push('/home')
-  }
-  </script>
+    <div class="mt-12">
+      <img src="https://www.svgrepo.com/show/382106/profile-avatar.svg"
+           alt="Profile"
+           class="w-32 h-32 rounded-full border-2 border-gray-600" />
+    </div>
+
+    <div class="mt-10 space-y-6 text-lg">
+      <p><span class="font-bold">Username:</span> {{ user.username }}</p><input type="text" class="bg-gray-700 text-white rounded-lg px-4 py-2" placeholder="Enter new username" />
+      <p><span class="font-bold">New Password</span> {{ user.password }}</p><input type="password" class="bg-gray-700 text-white rounded-lg px-4 py-2" placeholder="Enter new password" />
+      <p><span class="font-bold">Confirm New Password:</span> {{ user.password }}</p><input type="password" class="bg-gray-700 text-white rounded-lg px-4 py-2" placeholder="Confirm new password" />
+    </div>
+
+    <button class="mt-12 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-6 rounded-lg">
+      Account Delete
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import Navbar from '@/components/Navbar.vue';
+
+const user = {
+  pseudo: 'vincentleb',
+  name: 'Vincent',
+  surname: 'Lebel'
+}
+const router = useRouter()
+
+function goToHome() {
+  router.push('/home')
+}
+</script>
