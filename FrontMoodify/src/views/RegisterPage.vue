5c51d1c83141adb364023cb1e9e92dd2e0576543
Ace <ace92dev@gmail.com>
2025-04-15 11:57:03 +0200

    finalisation de la page register

--- FrontMoodify/src/views/RegisterPage.vue
+++ FrontMoodify/src/views/RegisterPage.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="min-h-screen flex items-center justify-center bg-black">
-      <div class="w-full max-w-sm text-center text-green-500 space-y-5">
-        <h1 class="text-4xl font-bold">Moodify</h1>
+      <div class="text-center w-[400px] text-green-500 space-y-5 bg-[#1e1e1e] p-10 rounded-xl">
+        <h1 class="text-4xl font-bold"  id="moodify">Moodify</h1>
   
         <input
           v-model="username"
           type="text"
           placeholder="Username"
-          class="w-full border border-green-500 rounded-lg py-2 px-4 bg-black text-green-500 placeholder-green-500"
+          class="w-full border border-green-500 rounded-lg py-2 px-4 text-green-500 placeholder-green-500"
         />
         <input
           v-model="email"
           type="text"
           placeholder="Email"
-          class="w-full border border-green-500 rounded-lg py-2 px-4 bg-black text-green-500 placeholder-green-500"
+          class="w-full border border-green-500 rounded-lg py-2 px-4 text-green-500 placeholder-green-500"
         />
         <input
           v-model="password"
           type="password"
           placeholder="Password"
-          class="w-full border border-green-500 rounded-lg py-2 px-4 bg-black text-green-500 placeholder-green-500"
+          class="w-full border border-green-500 rounded-lg py-2 px-4 text-green-500 placeholder-green-500"
         />
         <input
           v-model="confirmPassword"
           type="password"
           placeholder="Confirm Password"
-          class="w-full border border-green-500 rounded-lg py-2 px-4 bg-black text-green-500 placeholder-green-500"
+          class="w-full border border-green-500 rounded-lg py-2 px-4 text-green-500 placeholder-green-500"
         />
   
         <button
@@ -34,6 +34,20 @@
         >
           Register
         </button>
+
+        <div class="flex items-center justify-center space-x-4">
+        <hr class="border-green-500 flex-1" />
+        <span class="text-green-500">or</span>
+        <hr class="border-green-500 flex-1" />
+      </div>
+
+      <button
+          @click="goToLogin"
+          class="w-full bg-green-500 text-black font-semibold py-2 rounded-lg hover:bg-green-600 transition"
+        >
+          Log In
+        </button>
+
       </div>
     </div>
   </template>
@@ -44,6 +58,10 @@
   import { register } from '@/services/authService'
   
   const router = useRouter()
+
+  function goToLogin() {
+    router.push('/')
+  }
   
   const username = ref('')
   const email = ref('')
