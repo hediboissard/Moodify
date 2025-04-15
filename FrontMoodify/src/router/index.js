7e03a2af84b0d70471625a2145e621849dff9e87
Ace <ace92dev@gmail.com>
2025-04-15 09:26:13 +0200

    fix conflict

--- FrontMoodify/src/router/index.js
+++ FrontMoodify/src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../views/Home.vue";
 import Friends from "../views/Friends.vue";
 import LoginPage from "../views/LoginPage.vue";
 import RegisterPage from '../views/RegisterPage.vue'
+import ProfilPage from '../views/ProfilPage.vue'
 
 const routes = [
   { path: "/", name: "Login", component: LoginPage },
