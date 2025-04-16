fd0c7b39ac77127ec6ad807cd90503d013133a6b
nubroc <numa.brocandel@efrei.net>
2025-04-16 12:03:55 +0200

    sql

--- BackMoodify/src/script.sql
+++ BackMoodify/src/script.sql
@@ -1,16 +1,11 @@
--- Création de la base de données
+
 CREATE DATABASE IF NOT EXISTS moodify;
 
 USE moodify;
 
--- Supprimer la table users si elle existe
+
 DROP TABLE IF EXISTS users;
 
-<<<<<<< HEAD
-
-=======
---Création de la table users
->>>>>>> dcd264caa16348ee3a1ce0363cee1e20fa96af03
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(191) NOT NULL UNIQUE,
@@ -20,8 +15,4 @@ CREATE TABLE users (
   mood VARCHAR(50),
   avatar VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> dcd264caa16348ee3a1ce0363cee1e20fa96af03
