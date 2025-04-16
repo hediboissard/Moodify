81c5898f4d8a413c9a098686bd959cded8c24d08
Vincent <vincent.lebel@efrei.net>
2025-04-16 09:47:24 +0200

    feat: modify users table to make password nullable and add avatar column

--- BackMoodify/src/script.sql
+++ BackMoodify/src/script.sql
@@ -1,17 +1,18 @@
+-- Création de la base de données
 CREATE DATABASE IF NOT EXISTS moodify;
 USE moodify;
 
+-- Supprimer la table users si elle existe
 DROP TABLE IF EXISTS users;
 
+--Création de la table users
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(191) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255),
   username VARCHAR(100) NOT NULL,
   spotify_id VARCHAR(255),
   mood VARCHAR(50),
+  avatar VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Ajouter la colonne avatar à la table users
-ALTER TABLE users ADD COLUMN avatar VARCHAR(255);
