e6d5919f7129d2110ecc8da3afc9115853967144
Jules Dupont <jules.dupont0709@gmail.com>
2025-04-17 09:41:08 +0200

    Replace Spotify logo with SVG and add playlist addition button in dropdown menu

--- FrontMoodify/src/views/HomePage.vue
+++ FrontMoodify/src/views/HomePage.vue
@@ -23,11 +23,18 @@
             <button class="more-btn" @click="toggleMenu(index)">⋯</button>
             <div v-if="openMenuIndex === index" class="dropdown-menu">
               <a :href="song.spotify_url" target="_blank" class="dropdown-link">
-                <img src="../../public/logo_spotify.svg" alt="Spotify" class="dropdown-icon" />
+                <svg class="w-5 h-5" viewBox="0 0 168 168" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M84,0C37.7,0,0,37.7,0,84s37.7,84,84,84s84-37.7,84-84S130.3,0,84,0z M121.3,120.1c-1.5,2.5-4.7,3.3-7.2,1.8
+                  c-19.8-12.1-44.8-14.9-74.2-8.4c-2.8,0.6-5.5-1.2-6.1-4s1.2-5.5,4-6.1c32.3-7,60.1-3.7,82.2,10.1
+                  C121.9,114.2,122.7,117.5,121.3,120.1z M133.4,97.6c-1.9,3-5.8,3.9-8.7,2c-22.7-14-57.3-18-84.1-10.1c-3.4,1-6.9-0.9-7.9-4.3
+                  c-1-3.4,0.9-6.9,4.3-7.9c31.8-9.2,70.5-4.7,97.5,11.6C134.3,90.8,135.2,94.7,133.4,97.6z M134.9,74.1c-27-16.2-71.5-17.7-97-10
+                  c-4,1.2-8.2-1-9.4-5c-1.2-4,1-8.2,5-9.4c29.6-8.8,79-7.1,110.9,11.7c3.6,2.1,4.8,6.7,2.6,10.3C144.8,75.1,139.1,76.6,134.9,74.1z"/>
+              </svg>
                 Ouvrir dans Spotify
               </a>
 
               <button class="dropdown-link" @click="openPlaylistPopup(song)">
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-playlist-add"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 8h-14" /><path d="M5 12h9" /><path d="M11 16h-6" /><path d="M15 16h6" /><path d="M18 13v6" /></svg>
                 Ajouter à une playlist
               </button>
             </div>
