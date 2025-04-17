e5cf824d46a0b296e0154d6b5490d7ac81c6d7ef
Jules Dupont <jules.dupont0709@gmail.com>
2025-04-17 10:41:25 +0200

    ajout like et modif MusicPlayer

--- FrontMoodify/src/components/MusicPlayer.vue
+++ FrontMoodify/src/components/MusicPlayer.vue
@@ -25,18 +25,13 @@
         </div>
 
         <div class="controls">
-          <button @click.stop="prevTrack"> <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-       xmlns="http://www.w3.org/2000/svg" stroke="currentColor"
-       stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-    <path d="M15 18L9 12L15 6" />
-  </svg></button>
+          <button @click.stop="prevTrack"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevrons-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 7l-5 5l5 5" /><path d="M17 7l-5 5l5 5" /></svg></button>
           <button @click.stop="togglePlay">
-            {{ isPlaying ? '⏸️' : '▶️' }}
-          </button>
-          <button @click.stop="nextTrack"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-       stroke-linecap="round" stroke-linejoin="round">
-    <polyline points="9 18 15 12 9 6"></polyline>
-  </svg></button>
+            <svg v-if="isPlaying"  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /><path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /></svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" /></svg>
+        </button>
+
+          <button @click.stop="nextTrack"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevrons-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7l5 5l-5 5" /><path d="M13 7l5 5l-5 5" /></svg></button>
         </div>
       </div>
 
--- FrontMoodify/src/views/HomePage.vue
+++ FrontMoodify/src/views/HomePage.vue
@@ -10,7 +10,17 @@
       <p class="sidebar-title" @click="toggleLikedExpanded" style="cursor: pointer;">
         <span v-if="likedExpanded">⬇️</span>
         <span v-else>➡️</span>
-        ❤️ Titres Likés
+        <span v-else>➡️</span>
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="20" height="20"
+     viewBox="0 0 24 24"
+     fill="currentColor"
+     class="icon icon-tabler icons-tabler-filled icon-tabler-heart"
+     style="vertical-align: middle; margin-right: 5px;">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
+</svg>
+Titres Likés
       </p>
       <div v-if="likedExpanded">
         <div v-for="(song, index) in likedSongs" :key="'liked-' + index" class="track-item">
@@ -87,8 +97,32 @@
           <p>{{ currentSong.artist }}</p>
         </div>
         <button @click="toggleLikeCurrentTrack" class="like-button">
-          {{ isLiked(currentSong) ? '❤️' : '🤍' }}
-        </button>
+  <!-- Cœur rempli (liké) -->
+  <svg v-if="isLiked(currentSong)"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24" height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    class="icon icon-tabler icons-tabler-filled icon-tabler-heart">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" />
+  </svg>
+
+  <!-- Cœur vide (non liké) -->
+  <svg v-else
+    xmlns="http://www.w3.org/2000/svg"
+    width="24" height="24"
+    viewBox="0 0 24 24"
+    fill="none" stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="icon icon-tabler icons-tabler-outline icon-tabler-heart">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+  </svg>
+</button>
+
       </div>
     </div>
 
